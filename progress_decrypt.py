6bdb16962da4392f1a1420f37f5565077a41d4e0
strawp <github@strawp.net>
2021-02-02 09:49:12 +0000

    Added progress_decrypt.py - decrypt prowin32.exe encrypted credentials

---
+++ progress_decrypt.py
@@ -0,0 +1,28 @@
+# Decrypt prowin32.exe ASCII hex username / passwords that are "encrypted" when passed as startup parameters.
+# usage: python progress_decrypt.py oech1::deadbeefcafe
+
+import sys
+
+def decrypt( src ):
+    return do_crypto( bytearray.fromhex(src) )
+    
+def do_crypto( src ):
+    key_bytes = bytes('PROGRESS', 'utf-8')
+    key_len = len(key_bytes)
+    src_len = len( src )
+    dest_bytes = []
+    key_ct = 0
+
+    for ct in range(0, src_len ):
+       if key_ct >= key_len:
+         key_ct = 0
+       dest_bytes.append( src[ct] ^ key_bytes[key_ct] )
+       key_ct += 1 
+    return bytearray(dest_bytes).decode('utf-8');
+
+src = sys.argv[1]
+if src.startswith('oech1::'):
+    src = src.split(':')[3]
+
+print( decrypt( src ) )
+
